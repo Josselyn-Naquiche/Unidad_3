45e55e4c58be27dfe847a07ecbf6bfcb5e2f584c
Josselyn-Naquiche-ctc <josselynlunaa18@gmail.com>
2024-08-28 21:21:09 -0500

    nuevo

---
+++ Semana_8/Sesion_38/assets/js/script.js
@@ -0,0 +1,16 @@
+console.log("Hola mundo");
+
+let lista = [];
+console.log(lista);
+
+lista[0]=2;
+lista[1]=5;
+lista[2]=7;
+lista[3]=11;
+console.log(lista);
+
+console.log(lista[2]);
+console.log(lista[0]);
+
+lista[3]=15; //Cambio el elemento de la posición 3//
+console.log(lista);
---
+++ Semana_9/Sesion_41/assets/js/script.js
@@ -0,0 +1,11 @@
+// let number = [1,2,3,4];
+// number.push(5);
+// console.log(number);
+
+// let number2 = [1,4,6,8,3,7,6,11,1,609,2,6,'a'];
+// number2.pop();
+// console.log(number2);
+
+// let numeros = [2,5,7];
+// numeros.push(4);
+// console.log(numeros)
